c7fb6d694ab187d80a028b55fc63515457df00cf
rafaelfelipeac <rafaelfelipeac@gmail.com>
2018-12-05 16:16:49 -0200

    Using Navigation Architecture Component.

---
+++ app/src/main/java/com/rafaelfelipeac/readmore/ui/activities/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.rafaelfelipeac.readmore.ui.activities
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.rafaelfelipeac.readmore.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottom_nav, navController)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+}
