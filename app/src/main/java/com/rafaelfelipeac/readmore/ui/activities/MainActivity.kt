e919636a0fcd5aef2204ce44ee00a36c60b89b22
rafaelfelipeac <rafaelfelipeac@gmail.com>
2018-12-11 11:22:00 -0200

    Navigation for back arrow and save menu on GoalsFragment and GoalFormFragment.

--- app/src/main/java/com/rafaelfelipeac/readmore/ui/activities/MainActivity.kt
+++ app/src/main/java/com/rafaelfelipeac/readmore/ui/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -27,8 +28,27 @@ class MainActivity : BaseActivity() {
 
         supportActionBar?.elevation = 0F
 
-        toolbar?.navigationIcon?.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP)
+        toolbar?.navigationIcon?.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary),
+            PorterDuff.Mode.SRC_ATOP)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.menu_goal_save -> onBackPressed()
+            android.R.id.home -> onBackPressed()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        clearToolbarMenu()
+    }
+
+    private fun clearToolbarMenu() {
+        toolbar.menu.clear()
+    }
 }
--- app/src/main/java/com/rafaelfelipeac/readmore/ui/fragments/GoalFormFragment.kt
+++ app/src/main/java/com/rafaelfelipeac/readmore/ui/fragments/GoalFormFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 
 import com.rafaelfelipeac.readmore.R
 import com.rafaelfelipeac.readmore.ui.activities.MainActivity
 import com.rafaelfelipeac.readmore.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class GoalFormFragment : BaseFragment() {
 
@@ -19,6 +21,8 @@ class GoalFormFragment : BaseFragment() {
     ): View? {
         (activity as AppCompatActivity).supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         (activity as MainActivity).supportActionBar?.title = "GoalForm"
+
+        (activity as MainActivity).toolbar.inflateMenu(R.menu.menu_save)
 
         return inflater.inflate(R.layout.fragment_goal_form, container, false)
     }
