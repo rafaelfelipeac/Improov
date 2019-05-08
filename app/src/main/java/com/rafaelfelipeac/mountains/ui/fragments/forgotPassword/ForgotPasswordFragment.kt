a433093f1dac29b412130835f8894073a03e14c2
Rafael Cordeiro <rafaelfelipeac@gmail.com>
2019-05-08 17:40:14 -0300

    Created ForgotPasswordFragment, improvements in layouts and extracting some strings.

---
+++ app/src/main/java/com/rafaelfelipeac/mountains/ui/fragments/forgotPassword/ForgotPasswordFragment.kt
@@ -0,0 +1,32 @@
+package com.rafaelfelipeac.mountains.ui.fragments.forgotPassword
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.rafaelfelipeac.mountains.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class ForgotPasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+
+}
