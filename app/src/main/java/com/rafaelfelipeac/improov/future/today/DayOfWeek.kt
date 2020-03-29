fa6965ad39ab36dba46845128728a589e7d285a0
Rafael <rafaelfelipeac@gmail.com>
2020-03-29 17:32:53 -0300

    Code for future is removed.

--- app/src/main/java/com/rafaelfelipeac/improov/future/today/DayOfWeek.kt
+++
@@ -1,9 +0,0 @@
-package com.rafaelfelipeac.improov.future.today
-
-import com.rafaelfelipeac.improov.future.habit.Habit
-
-data class DayOfWeek(
-    var weekDay: String = "",
-    val monthDay: String = "",
-    var list: MutableList<Habit> = mutableListOf()
-)
