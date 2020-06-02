9a53a0ebe15a9a9c759129080ca52623157cd1be
Rafael <rafaelfelipeac@gmail.com>
2020-06-01 22:39:38 -0300

    Added structure for more tests on Goal package.

---
+++ app/src/test/java/com/rafaelfelipeac/improov/features/goal/domain/usecase/firsttimeadd/GetFirstTimeAddUseCaseTest.kt
@@ -0,0 +1,9 @@
+package com.rafaelfelipeac.improov.features.goal.domain.usecase.firsttimeadd
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetFirstTimeAddUseCaseTest {
+
+}
---
+++ app/src/test/java/com/rafaelfelipeac/improov/features/goal/domain/usecase/firsttimelist/SaveFirstTimeListUseCase.kt
@@ -0,0 +1,9 @@
+package com.rafaelfelipeac.improov.features.goal.domain.usecase.firsttimelist
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class SaveFirstTimeListUseCase {
+
+}
