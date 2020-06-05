cf704027320f20cf874b991c26afca64c316c730
Rafael <rafaelfelipeac@gmail.com>
2020-06-04 23:07:12 -0300

    Unit tests for Welcome.

--- app/src/test/java/com/rafaelfelipeac/improov/features/welcome/presentation/WelcomeViewModelTest.kt
+++ app/src/test/java/com/rafaelfelipeac/improov/features/welcome/presentation/WelcomeViewModelTest.kt
@@ -1,9 +1,76 @@
 package com.rafaelfelipeac.improov.features.welcome.presentation
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.rafaelfelipeac.improov.base.CoroutineRule
+import com.rafaelfelipeac.improov.base.DataProvider.shouldBeEqualTo
+import com.rafaelfelipeac.improov.features.welcome.domain.usecase.GetWelcomeUseCase
+import com.rafaelfelipeac.improov.features.welcome.domain.usecase.SaveWelcomeUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito
+import org.mockito.BDDMockito.given
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class WelcomeViewModelTest {
 
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var coroutinesTestRule = CoroutineRule()
+
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
+
+    private var mockSaveWelcomeUseCase = BDDMockito.mock(SaveWelcomeUseCase::class.java)
+    private var mockGetWelcomeUseCase = BDDMockito.mock(GetWelcomeUseCase::class.java)
+
+    private lateinit var welcomeViewModel: WelcomeViewModel
+
+    @Before
+    fun setup() {
+        welcomeViewModel = WelcomeViewModel(
+            mockSaveWelcomeUseCase,
+            mockGetWelcomeUseCase
+        )
+    }
+
+    @Test
+    fun `GIVEN saveWelcome is successful WHEN onSaveWelcome is called THEN true is returned`() {
+        // given
+        val booleanValue = true
+
+        given(runBlocking { mockSaveWelcomeUseCase.execute(booleanValue) })
+            .willReturn(Unit)
+
+        // when
+        welcomeViewModel.onSaveWelcome(booleanValue)
+
+        // then
+        welcomeViewModel.stateLiveData.value shouldBeEqualTo WelcomeViewModel.ViewState(
+            welcomeSaved = true,
+            welcome = false
+        )
+    }
+
+    @Test
+    fun `GIVEN getWelcome is successful WHEN loadData is called THEN false is returned`() {
+        // given
+        val booleanValue = false
+
+        given(runBlocking { mockGetWelcomeUseCase.execute() })
+            .willReturn(booleanValue)
+
+        // when
+        welcomeViewModel.loadData()
+
+        // then
+        welcomeViewModel.stateLiveData.value shouldBeEqualTo WelcomeViewModel.ViewState(
+            welcomeSaved = false,
+            welcome = booleanValue
+        )
+    }
 }
