ac56d83159d41f5f1eb34157936098131a4296dd
Rafael <rafaelfelipeac@gmail.com>
2020-06-30 14:39:32 -0300

    SettingsViewModel.

--- app/src/main/java/com/rafaelfelipeac/improov/features/settings/presentation/settings/SettingsViewModel.kt
+++ app/src/main/java/com/rafaelfelipeac/improov/features/settings/presentation/settings/SettingsViewModel.kt
@@ -1,29 +1,6 @@
 package com.rafaelfelipeac.improov.features.settings.presentation.settings
 
-import com.rafaelfelipeac.improov.core.platform.base.BaseAction
-import com.rafaelfelipeac.improov.core.platform.base.BaseViewModel
-import com.rafaelfelipeac.improov.core.platform.base.BaseViewState
+import com.rafaelfelipeac.improov.core.platform.base.NewBaseViewModel
 import javax.inject.Inject
 
-class SettingsViewModel @Inject constructor() : BaseViewModel<SettingsViewModel.ViewState, SettingsViewModel.Action>(
-    ViewState()
-) {
-
-    override fun onLoadData() {
-        super.onLoadData()
-    }
-
-    override fun onReduceState(viewAction: Action) = when (viewAction) {
-        is Action.Settings -> state.copy(
-            settings = true
-        )
-    }
-
-    data class ViewState(
-        val settings: Boolean = false
-    ) : BaseViewState
-
-    sealed class Action : BaseAction {
-        object Settings : Action()
-    }
-}
+class SettingsViewModel @Inject constructor() : NewBaseViewModel()
