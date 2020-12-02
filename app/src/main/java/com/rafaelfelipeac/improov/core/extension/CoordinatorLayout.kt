b9cb1956e62b8ff3a9211075ed330b073b53d4d5
Rafael <rafaelfelipeac@gmail.com>
2020-12-01 22:28:51 -0300

    Height of BottomNavigation.

--- app/src/main/java/com/rafaelfelipeac/improov/core/extension/CoordinatorLayout.kt
+++ app/src/main/java/com/rafaelfelipeac/improov/core/extension/CoordinatorLayout.kt
@@ -8,8 +8,6 @@ import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.isVisible
 
-const val CL_HEIGHT = 350
-const val CL_HEIGHT_2 = CL_HEIGHT - 100
 const val CL_ANIMATION_DURATION: Int = 700
 
 var animatorNavigation: ValueAnimator? = null
@@ -18,11 +16,19 @@ var animatorNavigationFake: ValueAnimator? = null
 var showingNavigation = false
 var hidingNavigation = false
 
+var navigationHeight = 0
+var fakeNavigationHeight = 0
+
 fun CoordinatorLayout.show(fakeBottomNav: View) {
     if ((animatorNavigation?.isRunning == true) || hidingNavigation || isVisible) return
 
-    animatorNavigation = ValueAnimator.ofInt(0, CL_HEIGHT)
-    animatorNavigationFake = ValueAnimator.ofInt(0, CL_HEIGHT_2)
+    if (navigationHeight == 0 && fakeNavigationHeight == 0 && height > 0) {
+        navigationHeight = height
+        fakeNavigationHeight = (height * 0.66).toInt()
+    }
+
+    animatorNavigation = ValueAnimator.ofInt(0, navigationHeight)
+    animatorNavigationFake = ValueAnimator.ofInt(0, fakeNavigationHeight)
 
     animatorNavigation?.addUpdateListener { animation ->
         if (!hidingNavigation) {
@@ -72,8 +78,13 @@ fun CoordinatorLayout.show(fakeBottomNav: View) {
 fun CoordinatorLayout.hide(fakeBottomNav: View) {
     if ((animatorNavigation?.isRunning == true) || showingNavigation || !isVisible) return
 
-    animatorNavigation = ValueAnimator.ofInt(CL_HEIGHT, 0)
-    animatorNavigationFake = ValueAnimator.ofInt(CL_HEIGHT_2, 0)
+    if (navigationHeight == 0 && fakeNavigationHeight == 0 && height > 0) {
+        navigationHeight = height
+        fakeNavigationHeight = (height * 0.66).toInt()
+    }
+
+    animatorNavigation = ValueAnimator.ofInt(navigationHeight, 0)
+    animatorNavigationFake = ValueAnimator.ofInt(fakeNavigationHeight, 0)
 
     animatorNavigation?.addUpdateListener { animation ->
         if (!showingNavigation) {
