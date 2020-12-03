7439c3f89398bce80e8240cd996a3f3bc0c05b2d
Rafael <rafaelfelipeac@gmail.com>
2020-12-02 21:45:04 -0300

    Getting Toolbar's height.

--- app/src/main/java/com/rafaelfelipeac/improov/core/extension/Toolbar.kt
+++ app/src/main/java/com/rafaelfelipeac/improov/core/extension/Toolbar.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.appbar.AppBarLayout
 
-const val TOOLBAR_HEIGHT = 224
 const val TOOLBAR_ANIMATION_DURATION: Int = 700
 
 var animatorToolbar: ValueAnimator? = null
@@ -15,11 +14,17 @@ var animatorToolbar: ValueAnimator? = null
 var hidingToolbar = false
 var showingToolbar = false
 
+var toolbarHeight = 0
+
 fun Toolbar.show(supportActionBar: ActionBar?) {
     if ((animatorToolbar?.isRunning == true) || hidingToolbar || (supportActionBar?.isShowing != false))
         return
 
-    animatorToolbar = ValueAnimator.ofInt(0, TOOLBAR_HEIGHT)
+    if (toolbarHeight == 0 && height > 0) {
+        toolbarHeight = height
+    }
+
+    animatorToolbar = ValueAnimator.ofInt(0, toolbarHeight)
 
     animatorToolbar?.addUpdateListener { animation ->
         if (!hidingToolbar) {
@@ -52,7 +57,11 @@ fun Toolbar.hide(supportActionBar: ActionBar?) {
     if ((animatorToolbar?.isRunning == true) || showingToolbar || (supportActionBar?.isShowing != true))
         return
 
-    animatorToolbar = ValueAnimator.ofInt(TOOLBAR_HEIGHT, 0)
+    if (toolbarHeight == 0 && height > 0) {
+        toolbarHeight = height
+    }
+
+    animatorToolbar = ValueAnimator.ofInt(toolbarHeight, 0)
 
     animatorToolbar?.addUpdateListener { animation ->
         if (!showingToolbar) {
