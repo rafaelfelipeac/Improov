ae2e6bd82096d48add4695dd32cef089e331e774
Rafael <rafaelfelipeac@gmail.com>
2021-02-06 13:27:12 -0300

    Some improvements and added strikethrough effect on goal and item adapter.

--- app/src/main/java/com/rafaelfelipeac/improov/features/goal/presentation/goallist/GoalListAdapter.kt
+++ app/src/main/java/com/rafaelfelipeac/improov/features/goal/presentation/goallist/GoalListAdapter.kt
@@ -1,23 +1,21 @@
 package com.rafaelfelipeac.improov.features.goal.presentation.goallist
 
 import android.annotation.SuppressLint
+import android.graphics.Paint
 import android.util.DisplayMetrics
 import android.view.View
 import android.view.ViewTreeObserver
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.rafaelfelipeac.improov.R
+import com.rafaelfelipeac.improov.core.extension.visible
 import com.rafaelfelipeac.improov.core.extension.getNumberInExhibitionFormat
 import com.rafaelfelipeac.improov.core.extension.setWidthForProgress
 import com.rafaelfelipeac.improov.core.extension.formatToDayMonth
 import com.rafaelfelipeac.improov.core.platform.ActionCompleteContract
 import com.rafaelfelipeac.improov.core.platform.base.BaseAdapter
+import com.rafaelfelipeac.improov.databinding.ListItemGoalBinding
 import com.rafaelfelipeac.improov.features.commons.domain.model.Goal
 
-const val MARGIN_START = 10
 const val LOGICAL_DENSITY_MULTIPLIER = 4
 
 class GoalListAdapter(private val fragment: GoalListFragment) : BaseAdapter<Goal>(), ActionCompleteContract {
@@ -28,70 +26,57 @@ class GoalListAdapter(private val fragment: GoalListFragment) : BaseAdapter<Goal
 
     @SuppressLint("ClickableViewAccessibility")
     override fun View.bindView(item: Goal, viewHolder: ViewHolder) {
+        val binding = ListItemGoalBinding.bind(this)
+
         setOnClickListener { clickListener(item) }
 
-        val typeIcon = viewHolder.itemView.findViewById<ImageView>(R.id.itemGoalTypeIcon)
-        val title = viewHolder.itemView.findViewById<TextView>(R.id.itemGoalTitle)
-        val date = viewHolder.itemView.findViewById<TextView>(R.id.itemGoalDate)
-        val score = viewHolder.itemView.findViewById<TextView>(R.id.itemGoalScore)
-        val itemDrag = viewHolder.itemView.findViewById<ImageView>(R.id.itemGoalDragIcon)
-        val progressDone = viewHolder.itemView.findViewById<ImageView>(R.id.itemGoalProgressDone)
-        val progressTotal = viewHolder.itemView.findViewById<ImageView>(R.id.itemGoalProgressTotal)
-
-        title.text = item.name
+        binding.itemGoalTitle.text = item.name
 
         if (item.done) {
-            typeIcon.background = ContextCompat.getDrawable(fragment.requireContext(), R.mipmap.ic_item_done)
+            binding.itemGoalDoneIcon.visible()
 
-            val params = typeIcon.layoutParams as ConstraintLayout.LayoutParams
-            params.marginStart = MARGIN_START
-            typeIcon.layoutParams = params
-        } else {
-            typeIcon.background = ContextCompat.getDrawable(fragment.requireContext(), R.mipmap.ic_item_undone)
-
-            val params = typeIcon.layoutParams as ConstraintLayout.LayoutParams
-            params.marginStart = MARGIN_START
-            typeIcon.layoutParams = params
+            binding.itemGoalTitle.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
         }
 
         if (item.divideAndConquer) {
-            score.text = String.format(
+            binding.itemGoalScore.text = String.format(
                 "%s/%s",
                 item.value.getNumberInExhibitionFormat(),
                 item.goldValue.getNumberInExhibitionFormat()
             )
         } else {
-            score.text = String.format(
+            binding.itemGoalScore.text = String.format(
                 "%s/%s",
                 item.value.getNumberInExhibitionFormat(),
                 item.singleValue.getNumberInExhibitionFormat()
             )
         }
 
-        progressTotal.viewTreeObserver.addOnPreDrawListener(object :
-            ViewTreeObserver.OnPreDrawListener {
+        binding.itemGoalProgressTotal.viewTreeObserver
+            .addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
 
             override fun onPreDraw(): Boolean {
-                progressTotal.viewTreeObserver.removeOnPreDrawListener(this)
+                binding.itemGoalProgressTotal.viewTreeObserver.removeOnPreDrawListener(this)
 
                 val metrics = DisplayMetrics()
                 fragment.activity?.windowManager?.defaultDisplay?.getMetrics(metrics)
                 val logicalDensity = metrics.density
                 val margin = (logicalDensity * LOGICAL_DENSITY_MULTIPLIER).toInt()
 
-                progressDone.setWidthForProgress(item, progressTotal.measuredWidth - margin)
+                binding.itemGoalProgressDone
+                    .setWidthForProgress(item, binding.itemGoalProgressTotal.measuredWidth - margin)
 
                 return true
             }
         })
 
         if (item.date != null) {
-            date.text = item.date.formatToDayMonth(fragment.requireContext())
+            binding.itemGoalDate.text = item.date.formatToDayMonth(fragment.requireContext())
         } else {
-            date.text = ""
+            binding.itemGoalDate.text = ""
         }
 
-        itemDrag.setOnTouchListener { _, _ ->
+        binding.itemGoalDragIcon.setOnTouchListener { _, _ ->
             touchHelper?.startDrag(viewHolder)
             false
         }
