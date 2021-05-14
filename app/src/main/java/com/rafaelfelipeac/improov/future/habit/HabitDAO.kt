dddf344fe42e312350f9231eeb87e4c39e1fba15
Rafael <rafaelfelipeac@gmail.com>
2021-05-14 18:52:55 -0300

    Recover layouts.

--- app/src/main/java/com/rafaelfelipeac/improov/future/habit/HabitDAO.kt
+++ app/src/main/java/com/rafaelfelipeac/improov/future/habit/HabitDAO.kt
@@ -6,15 +6,15 @@ import androidx.room.*
 @Dao
 interface HabitDAO {
 
-//    @Query("SELECT * FROM habit")
-//    fun getAll(): LiveData<List<Habit>>
-//
-//    @Query("SELECT * FROM habit WHERE habitId = :habitId")
-//    fun get(habitId: Long): LiveData<Habit>
-//
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    fun save(habit: Habit): Long
-//
-//    @Delete
-//    fun delete(habit: Habit)
+        @Query("SELECT * FROM habit")
+    fun getAll(): LiveData<List<Habit>>
+
+    @Query("SELECT * FROM habit WHERE habitId = :habitId")
+    fun get(habitId: Long): LiveData<Habit>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun save(habit: Habit): Long
+
+    @Delete
+    fun delete(habit: Habit)
 }
